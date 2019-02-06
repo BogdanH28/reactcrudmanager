fa16b31fcaf79f758a49265f5e40fa82a3a750ba
Bogdan Heghes <boboh2895@gmail.com>
2019-02-06 16:32:53 +0200

    Updates

---
+++ precache-manifest.24ba6d0d5bd74add59179610065a015b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "909a959362159065f9ba",
+    "url": "/reactcrudmanager/static/css/main.da3b6268.chunk.css"
+  },
+  {
+    "revision": "909a959362159065f9ba",
+    "url": "/reactcrudmanager/static/js/main.909a9593.chunk.js"
+  },
+  {
+    "revision": "68913a6c54e9b8236e79",
+    "url": "/reactcrudmanager/static/css/1.415aa985.chunk.css"
+  },
+  {
+    "revision": "68913a6c54e9b8236e79",
+    "url": "/reactcrudmanager/static/js/1.68913a6c.chunk.js"
+  },
+  {
+    "revision": "7fc9677685ed265438b7",
+    "url": "/reactcrudmanager/static/js/runtime~main.7fc96776.js"
+  },
+  {
+    "revision": "c94f9ef6195277cf0967472ddde789ad",
+    "url": "/reactcrudmanager/index.html"
+  }
+];
